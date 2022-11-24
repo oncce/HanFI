ca18f25be9af45b052bed2519131211219948c38
oncce <78127460+oncce@users.noreply.github.com>
2022-11-24 20:44:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,6 +24,7 @@ echo 'src-git nas_istore https://github.com/linkease/istore.git;main' >> feeds.c
 echo 'src-git nas_isp https://github.com/linkease/istore-packages.git;main' >> feeds.conf.default
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git;master' >> feeds.conf.default
 echo 'src-git lede https://github.com/coolsnowwolf/lede.git;master' >>feeds.conf.default
+echo 'src-git luci https://github.com/openwrt/luci.git;main' >> feeds.conf.default
 echo 'src-git ntfs3 https://github.com/xymopen/openwrt-ntfs3.git;master' >>feeds.conf.default
 echo 'src-git netnasemusic https://github.com/maxlicheng/luci-app-unblockmusic.git;master' >>feeds.conf.default
 # echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' >>feeds.conf.default
